1b6eb744ba373ba17acd74b1f7935c6445edeffc
SosukeKato <dokindokinpiano@gmail.com>
2025-11-07 18:54:38 +0900

    [update]UIのButtonでSceneを切り替える処理を追加

--- Assets/_WorkSpace/Script/SceneController.cs
+++ Assets/_WorkSpace/Script/SceneController.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SceneChange(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
     }
 }
