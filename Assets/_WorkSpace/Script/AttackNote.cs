21272a010cc6b86b5d5b008646fc4882641154d8
SosukeKato <dokindokinpiano@gmail.com>
2025-11-10 05:31:24 +0900

    [update]Attackノーツのサイズが拡大する処理を追加

--- Assets/_WorkSpace/Script/AttackNote.cs
+++ Assets/_WorkSpace/Script/AttackNote.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class AttackNote : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    GameController _gc;
 
-    void Update()
+    [SerializeField]
+    float _playerAD = 200;
+    [SerializeField]
+    float _noteSpeed;
+    [SerializeField]
+    float _noteMaxSize = 3;
+
+    float _noteSize = 0;
+    void Awake()
     {
-        
+        _gc = FindAnyObjectByType<GameController>();
+        while (_noteSize <= _noteMaxSize)
+        {
+            _noteSize += _noteSpeed * Time.deltaTime;
+        }
     }
 }
