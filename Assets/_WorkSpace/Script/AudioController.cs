32568da5df0f4ac7951884d0d2431465d1ba097c
SosukeKato <dokindokinpiano@gmail.com>
2025-11-11 23:55:55 +0900

    [update]AudioManagerをシングルトンに変更

--- Assets/_WorkSpace/Script/AudioController.cs
+++ Assets/_WorkSpace/Script/AudioController.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
+    static AudioController instance { get;set; }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
         
