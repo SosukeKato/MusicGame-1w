e49dd3dd50077110350dcf962c6c40b22692fd03
SosukeKato <dokindokinpiano@gmail.com>
2025-11-12 04:41:06 +0900

    [update]poolを初期化する処理を追加

--- Assets/_WorkSpace/Script/Note/NotePool.cs
+++ Assets/_WorkSpace/Script/Note/NotePool.cs
@@ -1,7 +1,36 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NotePool : MonoBehaviour
 {
+    GameController _gc;
+    Transform _tr;
+
+    [SerializeField]
+    GameObject _notePrefab;
+    [SerializeField]
+    int _poolSize;
+
+    Transform _parent;
+
+    Queue<GameObject> _notePool = new Queue<GameObject>();
+
+    void Awake()
+    {
+        _gc = FindAnyObjectByType<GameController>();
+        InitializePool();
+    }
+
+    void InitializePool()
+    {
+        for (int i = 0; i < _poolSize; i++)
+        {
+            GameObject note = Instantiate(_notePrefab);
+            note.SetActive(false);
+            note.transform.SetParent(_parent);
+            _notePool.Enqueue(note);
+        }
+    }
     void Start()
     {
         
