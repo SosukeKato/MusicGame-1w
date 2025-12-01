6a0895ccd5fcbae17c70725858513347fc9e4099
SosukeKato <dokindokinpiano@gmail.com>
2025-12-01 21:30:34 +0900

    [update]SceneManagerをシングルトンに変更

--- Assets/_WorkSpace/Script/Manager/SceneController.cs
+++ Assets/_WorkSpace/Script/Manager/SceneController.cs
@@ -3,6 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    static SceneController instance { get; set; }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
         
