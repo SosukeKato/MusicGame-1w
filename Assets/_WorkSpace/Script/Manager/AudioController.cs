8f6ac987f0e53185702da4a6382a9b19678d3dd5
SosukeKato <dokindokinpiano@gmail.com>
2025-12-04 22:39:16 +0900

    [fix]publicにする必要がなかったためprivateに変更

--- Assets/_WorkSpace/Script/Manager/AudioController.cs
+++ Assets/_WorkSpace/Script/Manager/AudioController.cs
@@ -6,7 +6,7 @@ public class AudioController : MonoBehaviour
     static AudioController instance { get;set; }
 
     [SerializeField]
-    public AudioSource _as;
+    AudioSource _as;
 
     [SerializeField]
     AudioClip _titleBGM;
